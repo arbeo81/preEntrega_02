a64988cdc89d2a046e902065fb3bf98afcc15430
arbeo81 <j.arbeo@gmail.com>
2023-07-25 14:46:02 -0300

    first version PreEntrega 02

---
+++ src/router/views.router.js
@@ -0,0 +1,91 @@
+import { Router } from "express";
+import { ChatDataBase } from "../dao/dbManagers/chat.js";
+import { ProductsDataBase } from "../dao/dbManagers/products.js";
+import { CartsDataBase } from "../dao/dbManagers/carts.js";
+
+const router = Router();
+const productDataBase = new ProductsDataBase();
+const chatDataBase = new ChatDataBase();
+const cartsDataBase = new CartsDataBase();
+
+router.get("/products", async (req, res) => {
+    const { page = 1 } = req.query;
+    const limit = req.query.limit;
+    const query = req.query;
+    const {
+        payload: products,
+        hasPrevPage,
+        hasNextPage,
+        nextPage,
+        prevPage,
+    } = await productDataBase.getProducts(limit, page, query);
+
+    return res.render("products", {
+        products: products,
+        page,
+        hasPrevPage,
+        hasNextPage,
+        prevPage,
+        nextPage,
+    });
+
+});
+
+router.get("/product/:pid", async (req, res) => {
+    try {
+        const pid = req.params.pid;
+        const producto = await productDataBase.getProductById(pid);
+        return res.render("product", { title: "Product", producto: producto });
+    } catch (error) {
+        console.error(error);
+        return res.status(404).send({ status: "Error", error: "No se encontró el producto seleccionado" });
+    }
+});
+
+router.get("/cart/:cid", async (req, res) => {
+    const cid = req.params.cid;
+    const cart = await cartsDataBase.getCartById(cid);
+    if (!cart) {
+        return res.status(400).send({ status: "Error", error: "Carrito no encontrado" });
+    } else {
+        return res.render("cart", { title: "Cart", cart: cart });
+    }
+});
+
+router.get("/realtimeproducts", async (req, res) => {
+    const { page = 1 } = req.query;
+    const limit = req.query.limit;
+    const query = req.query;
+    const productos = await productDataBase.getProducts(limit, page, query);
+    res.render("realtimeproducts", { title: "realtimeproducts", productos: productos });
+});
+
+router.get("/", async (req, res) => {
+    const { page = 1 } = req.query;
+    const limit = req.query.limit;
+    const query = req.query;
+    const {
+        payload: products,
+        hasPrevPage,
+        hasNextPage,
+        nextPage,
+        prevPage,
+    } = await productDataBase.getProducts(limit, page, query);
+
+    return res.render("home", {
+        products: products,
+        page,
+        hasPrevPage,
+        hasNextPage,
+        prevPage,
+        nextPage,
+    });
+});
+
+router.get("/chat", async (req, res) => {
+    const messages = await chatDataBase.getMessages();
+    res.render("chat", { title: "chat", messages: messages });
+});
+
+
+export default router;
